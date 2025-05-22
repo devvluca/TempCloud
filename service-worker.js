7fdda0a1446d373647aa4411d62228879af645cb
Luca <lucanobre1@gmail.com>
2025-05-22 15:01:49 -0300

    deploy 1

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "monitoramento-cloud-v1";
+const toCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icone.png",
+  "./script.js",
+  "https://cdn.jsdelivr.net/npm/chart.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(toCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
